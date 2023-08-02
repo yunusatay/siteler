ed9a2bc86f463f1621438ec920dd2147464cd223
Yunus Emre Atay <99666761+yunusatay@users.noreply.github.com>
2023-08-02 07:26:26 +0300

    Add files via upload

---
+++ portfolio/calismalar/hali/hali.js
@@ -0,0 +1,12 @@
+
+function hesapla(){
+    var en = document.halii.en.value
+    var boy = document.halii.boy.value
+    var sonuc = en*boy*20
+    document.halii.sonuc.value = sonuc
+}
+function hesap(){
+var en=prompt("Lütfen Halınızın Enini Giriniz")
+var boy=prompt("Lütfen Halınızın Boyunu Giriniz")
+var sonuc=alert("Halınızın Fiyatı " + en*boy)
+}
